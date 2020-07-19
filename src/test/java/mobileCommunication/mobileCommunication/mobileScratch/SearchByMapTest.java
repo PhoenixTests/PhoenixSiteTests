4c5cb3d630d2a62a7b77d445779385d5bf5a2024
Julie <mishacollins68@gmail.com>
2020-07-20 00:27:45 +0300

    feat: Добавлены тесты к странице Пополнение счета

---
+++ src/test/java/mobileCommunication/mobileCommunication/mobileScratch/SearchByMapTest.java
@@ -0,0 +1,87 @@
+package mobileCommunication.mobileCommunication.mobileScratch;
+
+import common.CommonPageActions;
+import common.CommonSteps;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchByMapTest extends DefaultMobileScratchSteps{
+    @Step("Нажать на кнопку 'Найти ближайшую точку продажи'")
+    public void clickOnClosestSalePointButton() {
+        mobileScratchPage.getFindClosestSalePointButton().click();
+    }
+
+    @Step("Выбрать из списка точку продаж скретч-карт")
+    public String selectFromSalePointsTable() {
+        mobileScratchPage.setByScratchAddresses(mobileScratchPage.getByScratchSelectCity().getSelectedValue());
+        mobileScratchPage.setByScratchCitySalePoints();
+        return mobileScratchPage.getByScratchCitySalePoints()
+                .get(random.nextInt(mobileScratchPage.getByScratchCitySalePoints().size()))
+                .getText();
+    }
+
+    @Step("Ввести в поле адрес '{address}'")
+    public void inputAddress(String address) {
+        mobileScratchPage.getUserAddress().setValue(address);
+    }
+
+    @Step("Нажать на кнопку 'Найти ближайшую точку продажи'")
+    public void checkIsUserAddressTextFieldDisplayed() {
+        CommonPageActions.switchToFrame(mobileScratchPage.getMapFrame());
+        mobileScratchPage.checkIsDisplayed(mobileScratchPage.getUserAddress());
+    }
+
+    @Step("Нажать на кнопку 'Найти'")
+    public void clickOnFindOnMapButton() {
+        mobileScratchPage.getFindOnMapButton().click();
+    }
+
+    @Step("Проверить, что появилась карта")
+    public void checkIsMapDisplayed() {
+        assertTrue(mobileScratchPage.checkIsDisplayed(mobileScratchPage.getMap()));
+    }
+
+    @Step("Нажать на 'x' чтобы закрыть окно с картой")
+    public void closeMapWindow() {
+        CommonPageActions.exitFromFrame();
+        mobileScratchPage.getMapCloser().click();
+    }
+
+    @Step("Проверить, что окно с картой закрылось")
+    public void checkIsMapWindowClosed() {
+        assertTrue(mobileScratchPage.checkIsNotDisplayed(mobileScratchPage.getMapFrame()));
+    }
+
+    @BeforeAll
+    public static void openPage() {
+        random=new Random();
+        mobileScratchPage=new MobileScratchPage();
+        CommonSteps.openPage(MobileScratchPage.getPageName(), MobileScratchPage.getURL());
+    }
+
+    @Test
+    @DisplayName("Поиск ближайшей точки продажи скретч-карт")
+    @Description("Тест на корректную работу всплывающего окна с картой")
+    @Severity(SeverityLevel.MINOR)
+    public void shouldSearchByMapWindow() {
+        clickOnRefillByScratch();
+        selectCityByScratch();
+        String address=selectFromSalePointsTable();
+        clickOnClosestSalePointButton();
+        checkIsUserAddressTextFieldDisplayed();
+        inputAddress(address);
+        clickOnFindOnMapButton();
+        checkIsMapDisplayed();
+        closeMapWindow();
+        checkIsMapWindowClosed();
+    }
+}
