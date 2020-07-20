0525620ed17c55c876ece1e4a2e080529f17d7f4
RozanovaSvetlana <khitrik.rozanova.svetlana@gmail.com>
2020-07-20 11:42:35 +0300

    feat: добавлены шаги переключения на фрейм и выход из фрейма

--- src/test/java/common/CommonSteps.java
+++ src/test/java/common/CommonSteps.java
@@ -2,6 +2,8 @@ package common;
 
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommonSteps {
@@ -23,5 +25,15 @@ public class CommonSteps {
     @Step("Сравнение url только что открытой вкладки с {url}")
     public static void checkIsCorrectUrl(String url) {
         assertEquals(url, CommonPageActions.getCurrentURL());
+    }
+
+    @Step("Переход на открывшийся фрейм")
+    public static void switchFrame() {
+        CommonPageActions.switchToFrame($(byId("popup-iframe")));
+    }
+
+    @Step("Выход из фрейма")
+    public static void exitFrame() {
+        CommonPageActions.exitFromFrame();
     }
 }
