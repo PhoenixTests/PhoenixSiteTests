67ccaf11f269bc75e83ed2edaea24b25ff285152
Julie <mishacollins68@gmail.com>
2020-07-20 19:02:06 +0300

    feat: Добавлены тесты к странице Домашний интернет - настройка компьютера

---
+++ src/main/java/internet/setup/SetupPage.java
@@ -0,0 +1,27 @@
+package internet.setup;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.Data;
+import lombok.Getter;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+
+@Data
+public class SetupPage {
+
+    @Getter
+    private static String URL = "http://phoenix-dnr.ru/internet-setup.php?";
+    @Getter
+    private static String pageName = "Настройка интернета";
+
+    private SelenideElement buttonsGroup = $(byId("hexagon_internet_setup"));
+    private SelenideElement OSButton;
+    private SelenideElement instructionTitle=$(byId("text")).$(By.tagName("h3"));
+
+    public SelenideElement getOSButton(String hrefValue) {
+        return buttonsGroup.$(byAttribute("href", hrefValue));
+    }
+}
---
+++ src/test/java/internet/setup/SetupPageTest.java
@@ -0,0 +1,56 @@
+package internet.setup;
+
+import com.tngtech.junit.dataprovider.DataProvider;
+import com.tngtech.junit.dataprovider.DataProviderExtension;
+import com.tngtech.junit.dataprovider.UseDataProvider;
+import com.tngtech.junit.dataprovider.UseDataProviderExtension;
+import common.CommonSteps;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(UseDataProviderExtension.class)
+@ExtendWith(DataProviderExtension.class)
+@Epic("Раздел 'Настройка интернета'")
+@Feature("Тестирование кнопок, предназначеных для отображения инструкций по настройке интернета на различных ОС")
+public class SetupPageTest {
+    private SetupPage setupPage;
+
+    @Step("Нажать на кнопку '{buttonName}'")
+    public void clickOnOSButton(String buttonName, String hrefValue) {
+        setupPage.getOSButton(hrefValue).click();
+    }
+
+    @Step("Проверить, что отобразилась инструкция, в заголовке которой есть ОС {OS}")
+    public void checkIsInstructionDisplayed(String OS) {
+        assertTrue(setupPage.getInstructionTitle().getText().contains(OS));
+    }
+
+    @DataProvider
+    public static Object[][] setup() {
+        return new Object[][]{
+                {"?setup=1", "Windows XP"},
+                {"?setup=2", "Windows Vista"},
+                {"?setup=3", "Windows 7"},
+                {"?setup=5", "Windows 10"},
+                {"?setup=6", "Mac OS"},
+                {"?setup=7", "Ubuntu"},
+        };
+    }
+
+    @TestTemplate
+    @UseDataProvider("setup")
+    @DisplayName("Настройка интернета на различных ОС")
+    @Description("Тест на работоспособность кнопок для отображения инструкции по настройке интернета")
+    @Severity(SeverityLevel.MINOR)
+    public void shouldShowSetupInstruction(String url, String OS) {
+        setupPage=new SetupPage();
+        CommonSteps.openPage(SetupPage.getPageName(),SetupPage.getURL());
+        clickOnOSButton(OS, url);
+        checkIsInstructionDisplayed(OS);
+    }
+
+}
