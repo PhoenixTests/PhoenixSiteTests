ec0c8cb8935da72e3cb8985f0ae9c4d9735efbc4
Julie <mishacollins68@gmail.com>
2020-07-22 14:29:34 +0300

    improvement: класс теперь абстрактный, убраны SearchTest параметры в методах. Исправлены assert'ы

--- src/test/java/search/SearchSteps.java
+++ src/test/java/search/SearchSteps.java
@@ -2,54 +2,55 @@ package search;
 
 import io.qameta.allure.Step;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-public class SearchSteps {
+public abstract class SearchSteps {
+    protected static SearchPage searchPage;
 
     @Step("Нажимаем на значок поиска")
-    public static void clickSearch(SearchPage searchPage) {
+    public static void clickSearch() {
         searchPage.getButtonSearch().click();
     }
 
     @Step("Ставим курсор на поле ввода")
-    public static void clickInput(SearchPage searchPage) {
+    public static void clickInput() {
         searchPage.getInputSearch().click();
     }
 
-    @Step("Ввод текст - \"{1}\" и осуществляем поиск")
-    public static void textInput(SearchPage searchPage, String text) {
+    @Step("Ввод текст - \"{0}\" и осуществляем поиск")
+    public static void textInput(String text) {
         searchPage.getInputSearch().setValue(text);
     }
 
-    @Step("Ищем среди найденных результатов ссылку - \"{1}\" на нужный раздел")
-    public static void checkLink(SearchPage searchPage, String text) {
+    @Step("Ищем среди найденных результатов ссылку - \"{0}\" на нужный раздел")
+    public static void checkLink(String text) {
         searchPage.setLinkFind(text);
-        assertEquals(true, searchPage.getLinkFind().exists());
+        assertTrue(searchPage.getLinkFind().exists());
     }
 
     @Step("Переходим по найденной ссылке")
-    public static void clickLink(SearchPage searchPage) {
+    public static void clickLink() {
         searchPage.getLinkFind().click();
     }
 
     @Step("Открываем окно статистики")
-    public static void clickStatistics(SearchPage searchPage) {
+    public static void clickStatistics() {
         searchPage.getStatisticFind().click();
     }
 
     @Step("Переходим по ссылке сайта")
-    public static void clickLinkWebsite(SearchPage searchPage) {
+    public static void clickLinkWebsite() {
         searchPage.getWebsite().click();
     }
 
     @Step("Проверяем, доступен ли сайт")
-    public static void checkWebsite(SearchPage searchPage) {
-        assertEquals(false, searchPage.getExceptionSite().exists());
+    public static void checkWebsite() {
+        assertFalse(searchPage.getExceptionSite().exists());
     }
 
     @Step("Закрываем статистику")
-    public static void closeWebsite(SearchPage searchPage) {
+    public static void closeWebsite() {
         searchPage.getButtonClose().click();
     }
 
